641f26c10d37814d9748d8f432f827f8424f80d5
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-01-20 15:45:06 +0200

    added guess form

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,19 +1,28 @@
 class ExamplesController < ApplicationController
-  def name
+  def name_query
     @name = params[:name]
     render "name.html.erb"
   end
 
-  def guess
+  def guess_query_action
     
-    @guess = params[:guess].to_i
+    @guess_query = params[:guess].to_i
     render "guess.html.erb"
   end
   
-  def guess2
+  def guess_segment_action
     
-    @guess2 = params[:your_guess].to_i
-    render "guess2.html.erb"
+    @guess_segment = params[:your_guess].to_i
+    render "guess_2.html.erb"
+  end
+
+  def guess_form_action
+    render "form.html.erb"
+  end
+
+  def guess_form_result_action
+    @message = params[:form_message].to_i
+    render "form_result.html.erb"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get "/name", to: 'examples#name'
-  get "/guess", to: 'examples#guess'
-  get "/guess2/:your_guess", to: 'examples#guess'
+  get "/name_query", to: 'examples#name_action'
+  get "/guess_query", to: 'examples#guess_query_action'
+  get "/guess_segment/:your_guess", to: 'examples#guess_segment_action'
+
+  get "/guess_form", to: 'examples#guess_form_action'
+  post "/guess_form_result", to: 'examples#guess_form_result_action'
 end
