c92a3f127a04fe8022b816f6ca1249b1da93a546
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-01-20 12:31:07 +0200

    added segment params

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,15 +1,19 @@
 class ExamplesController < ApplicationController
   def name
     @name = params[:name]
-    puts "below are your names"
-    puts @name
     render "name.html.erb"
   end
 
   def guess
-    @guess = params[:guess]
-    puts "below is your guess"
-    puts @guess
+    
+    @guess = params[:guess].to_i
     render "guess.html.erb"
   end
+  
+  def guess2
+    
+    @guess2 = params[:your_guess].to_i
+    render "guess2.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/name", to: 'examples#name'
   get "/guess", to: 'examples#guess'
+  get "/guess2/:your_guess", to: 'examples#guess'
 end
